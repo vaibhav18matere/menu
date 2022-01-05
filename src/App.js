6791334eccc2e960a5081f62c812eec74c94a70a
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2022-01-05 16:06:24 +0530

    feat: static menu added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React, { useState } from "react";
 import "./styles.css";
+import items from "./data";
+import Menu from "./Menu";
+import Categories from "./Categories";
 
 export default function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
   );
 }
---
+++ src/Categories.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <>
+      <h3>Categories component</h3>
+    </>
+  );
+};
+
+export default Categories;
