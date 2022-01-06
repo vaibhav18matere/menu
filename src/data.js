51a94eb23a2bec651a89ae29ebeb0f21ddc5a143
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2022-01-06 12:55:50 +0530

    feat : price added

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ const menu = [
     id: 1,
     title: "buttermilk pancakes",
     category: "breakfast",
-    price: 15.99,
+    price: 115.99,
     img: "./images/item-1.jpeg",
     desc: `A very light and fluffy pancake that requires fresh buttermilk, but it's the best I've ever made! `
   },
@@ -11,7 +11,7 @@ const menu = [
     id: 2,
     title: "diner double",
     category: "lunch",
-    price: 13.99,
+    price: 113.99,
     img: "./images/item-2.jpeg",
     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `
   },
@@ -19,7 +19,7 @@ const menu = [
     id: 3,
     title: "godzilla milkshake",
     category: "shakes",
-    price: 6.99,
+    price: 36.99,
     img: "./images/item-3.jpeg",
     desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`
   },
@@ -27,7 +27,7 @@ const menu = [
     id: 4,
     title: "country delight",
     category: "breakfast",
-    price: 20.99,
+    price: 120.99,
     img: "./images/item-4.jpeg",
     desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `
   },
@@ -43,7 +43,7 @@ const menu = [
     id: 6,
     title: "oreo dream",
     category: "shakes",
-    price: 18.99,
+    price: 148.99,
     img: "./images/item-6.jpeg",
     desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`
   },
@@ -51,7 +51,7 @@ const menu = [
     id: 7,
     title: "bacon overflow",
     category: "breakfast",
-    price: 8.99,
+    price: 48.99,
     img: "./images/item-7.jpeg",
     desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `
   },
@@ -59,7 +59,7 @@ const menu = [
     id: 8,
     title: "american classic",
     category: "lunch",
-    price: 12.99,
+    price: 92.99,
     img: "./images/item-8.jpeg",
     desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `
   },
@@ -67,7 +67,7 @@ const menu = [
     id: 9,
     title: "quarantine buddy",
     category: "shakes",
-    price: 16.99,
+    price: 86.99,
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`
   }
